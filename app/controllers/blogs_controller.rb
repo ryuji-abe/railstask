8a360dc2b21fe90a4b4009683273e337286e1028
ryuji abe <rabe714@gmail.com>
2018-01-08 22:04:53 +0900

    login task

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,14 +1,15 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
-  
+  before_action :require_login, only: [:new, :edit, :show]
+
   def index
     @blogs = Blog.all
   end
-  
+
   def new
     @blog = Blog.new
   end
-  
+
   def create
     @blog = Blog.create(blog_params)
     if @blog.save
@@ -17,11 +18,11 @@ class BlogsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
@@ -30,19 +31,29 @@ class BlogsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice:"ブログを削除しました！"
   end
-  
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
   def set_blog
     @blog = Blog.find(params[:id])
+  end
+
+  def require_login
+    unless logged_in?
+      redirect_to new_session_path
+    end
   end
 
 end
