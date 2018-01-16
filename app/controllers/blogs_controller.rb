bf026964802808bc0852ef558b9495ea8a222f2e
ryuji abe <rabe714@gmail.com>
2018-01-17 07:32:07 +0900

    association task

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,6 +12,7 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.create(blog_params)
+    @blog.user_id = current_user.id
     if @blog.save
       redirect_to blogs_path, notice:"ブログを作成しました！"
     else
@@ -39,6 +40,7 @@ class BlogsController < ApplicationController
 
   def show
     @blog = Blog.find(params[:id])
+    @favorite = current_user.favorites.find_by(blog_id: @blog.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :favorites, only: [:create, :destroy]
 end
