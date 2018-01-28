90e5ef9fa788c6009a154289440d2859f2218998
ryuji abe <rabe714@gmail.com>
2018-01-28 11:16:47 +0900

    delete debug log

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -3,7 +3,6 @@ class BlogMailer < ApplicationMailer
    @created_blog = created_blog
    user_info = User.find(@created_blog.user_id)
    user_email = user_info.email
-   puts user_email
 
    mail to: user_email, subject: "ブログ作成完了メール"
   end
